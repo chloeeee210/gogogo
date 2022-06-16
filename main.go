0595078ddc06572e2c1e98e0daf0eb50e7d00b04
chloeseo0210 <chloeseo0210@gmail.com>
2022-06-16 12:22:04 +0900

    add : getPage, getPages 구현

--- main.go
+++ main.go
@@ -4,33 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "http://programmers.co.kr/job?page=3"
+// https://www.udemy.com/courses/search/?p=1&q=golang&src=ukw
+var baseURL string = "https://gall.dcinside.com/board/lists/?id=tree"
 
 func main() {
-	getPages()
-	//fmt.Println(totalPages)
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
 }
 
-func getPages() int {
-	//pages := 0
-	res, err := http.Get(baseURL)
+func getPage(page int) {
+	pageUrl := baseURL + "&page=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageUrl)
+	res, err := http.Get(pageUrl)
 	checkErr(err)
 	checkCode(res)
 
 	defer res.Body.Close()
+
+}
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the title
-		title, err := s.Find("a").Html()
-		fmt.Printf(title)
-		checkErr(err)
+
+	doc.Find(".bottom_paging_wrap").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
 	})
-	return 0
+
+	return pages
 }
 
 func checkErr(err error) {
@@ -38,9 +50,9 @@ func checkErr(err error) {
 		log.Fatalln(err)
 	}
 }
+
 func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with Status:", res.StatusCode)
+		log.Fatalln("Request failed with StatusCode :", res.StatusCode)
 	}
-
 }
